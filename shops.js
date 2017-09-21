e1377931c006dabde017e285ecb9a4f3d94b477a
and85620 <and85620@gmail.com>
2017-09-21 17:00:06 +0800

    // 取全部店家資料

--- shops.js
+++ shops.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const shops = express.Router();
 
+/*
+shops.get('/', function (req, res) {
+    var sql2 = "select shop_name from shops";
+    connection.query( sql2, function(err, results) {
+      if (err) {
+        res.send("mysql getshops error");  
+      }
+      res.send(results);
+    });
+});
+*/
+
+// 取店家資料（所有相關（含菜單））
+shops.get('/', function (req, res) {
+    res.send(results);
+});
 
 module.exports = shops;
