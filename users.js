ff944326503e6863510c874dab7596acd57361dd
and85620 <and85620@gmail.com>
2017-09-21 17:01:25 +0800

    使用者 get patch post(註冊)

--- users.js
+++ users.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const users = express.Router();
 
+//取個人資料
+users.get('/', function (req, res) {
+    res.send(result);
+});
+
+//修改個人資料
+users.patch('/', function (req, res) {
+    res.send(result);
+});
+
+//註冊
+users.post('/', function (req, res) {
+    res.send(result);
+});
 
 module.exports = users;
