3bd25ac22a463fb79277420b25ce823d2fc6d1af
Unknown <and85620@gmail.com>
2017-09-23 00:08:24 +0800

    user註冊 -- function

--- models/user.js
+++ models/user.js
@@ -11,13 +11,13 @@
 //     console.log(results);
 // });
 
-function validate (username, password, callback) {
-    if (typeof(username) !== 'string' || typeof(password) !== 'string') {
+function validate (username, password, callback) { // 登入驗證
+    if (typeof(username) !== 'string' || typeof(password) !== 'string') { // 帳密要求字串型態
         let error='username and password must be string.';
         callback(error, undefined);
         return;
     }
-    let sql = `select user_id from users where user_id='${username}' and password='${password}';`;
+    let sql = `select user_id from users where user_id='${username}' and password='${password}';`;  // sql指令 -> 確認帳密
     // console.log(sql);
     connection.query(sql, (err, results, fields) => {
         if (err) {
@@ -32,7 +32,30 @@ function validate (username, password, callback) {
         return;
     });
 }
+//module.exports = {validate};
+ module.exports.validate = validate;
 
 
-
-module.exports.validate = validate;
+function register (username, password, callback) { // 註冊
+    if (typeof(username) !== 'string' || typeof(password) !== 'string') { // 帳密字串型態
+        let error='username and password must be string.';
+        callback(error, undefined);
+        return;
+    }
+    // sql指令 -> 增加新使用者
+    let sql = `INSERT INTO users (user_id , password , phone , email , money , name) VALUES ( '${username}' , '${password}' ,'298445637','ughoji',50,'李悅');`;  
+    connection.query(sql, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        // if (results.length > 0) {
+        //     let error = 'username exist.'
+        //     callback(error, undefined);
+        //     return;
+        // }
+        callback(undefined, { "ending" : "註冊成功" } );
+        return;
+    });
+}
+//module.exports = {register};
+module.exports.register = register;
