a562de90d14a3cbc1ee0b671b69fb18992d4e719
Unknown <and85620@gmail.com>
2017-09-23 00:12:08 +0800

    user註冊 -- insert 資料的api

--- usersRouter.js
+++ usersRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const usersRouter = express.Router();
+const user = require('./models/user');
+
 
 //取個人資料
 usersRouter.get('/', function (req, res) {
@@ -12,8 +14,26 @@ usersRouter.patch('/', function (req, res) {
 });
 
 //註冊
-usersRouter.post('/', function (req, res) {
-    res.send(result);
+usersRouter.post('/', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(400).json({
+            error: ' username or password ?? '
+        });
+    }
+
+    user.register(username, password, (error, results) => {
+
+        if (typeof(results) !== undefined && typeof(error) == "undefined") {
+            return res.status(200).json(results);
+        } else {
+            return res.status(401).json({
+                error: error
+            });
+        }
+    });
 });
 
 module.exports = usersRouter;
