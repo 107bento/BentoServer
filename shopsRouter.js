736e95560447b2e992975390f7802c2b8c405d28
Unknown <and85620@gmail.com>
2017-09-23 00:10:55 +0800

    拿店家資料（含店名,菜單）的api

--- shopsRouter.js
+++ shopsRouter.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const shopsRouter = express.Router();
+const shop = require('./models/shop');
 
-shopsRouter.get('/', function (req, res) {
-    
-});
 
 // 取店家資料（所有相關（含菜單））
-shopsRouter.get('/', function (req, res) {
-    res.send(results);
+shopsRouter.get('/', (req, res) => {
+
+    shop.showshop((error, results) => {
+
+        if (typeof(results) !== undefined && typeof(error) == "undefined") {
+            return res.status(200).json(results);
+        } else {
+            return res.status(401).json({
+                error: error
+            });
+        }
+    });
 });
 
 module.exports = shopsRouter;
