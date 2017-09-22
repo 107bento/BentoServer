1533abed2d6998d6a8cb84bd3adb6c8dc6d1b1e9
Unknown <and85620@gmail.com>
2017-09-23 00:07:39 +0800

    顯示店家data -- function

---
+++ models/shop.js
@@ -0,0 +1,20 @@
+function showshop (callback) { // 店家資料（＆菜單）
+    // sql指令 -> 所有shop data
+    let sql = `select shops.shop_id,shop_name,lowest_amount,highest_amount,shipping_fee,shop_discount,meals.meal_id,meal_name,meal_price,meal_discount
+    from shops, meals
+    where shops.shop_id = meals.shop_id
+    Group by  meals.meal_id;`;  
+    connection.query(sql, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        console.log(results);
+        /*data = {
+
+        }*/
+        callback(undefined, { "ending" : "店家顯示ok" } );
+        return;
+    });
+}
+//module.exports = {showshop};
+module.exports.showshop = showshop;
