08f3bb997d5067b2af19752093ab16370d0534c3
An-Shiang Tsai <ast850328@gmail.com>
2017-09-24 19:12:25 +0800

    登入的時候，會存入使用者資訊在 server 端，並把設置 res.cookie

--- loginRouter.js
+++ loginRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const loginRouter = express.Router();
 const user = require('./models/user');
+const moment = require('moment');
+const md5 = require('md5');
 
 loginRouter.post('/', (req, res) => {
 
@@ -14,8 +16,12 @@ loginRouter.post('/', (req, res) => {
     }
 
     user.validate(username, password, (error, results) => {
-
         if (typeof(results) !== undefined && typeof(error) == "undefined") {
+            // session id 加密 （username + 時間戳ㄋ）
+            let uuid = md5(results.user_id + moment());
+            //
+            _cookies[uuid] = results.user_id;
+            res.cookie(cookieName, uuid);
             return res.status(200).json(results);
         } else {
             return res.status(401).json({
