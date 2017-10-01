1c6b866d89ccad04ac90b60e9ad42ef57ab74ab0
SafeFly <ast850328@gmail.com>
2017-10-01 17:19:53 +0800

    clean code & update

--- models/order.js
+++ models/order.js
@@ -10,9 +10,9 @@ function newOrder (username, orderTime, total, details, callback) {
         //console.log(results.insertId);
         let orderId = results.insertId;
         for (let detail of details) {
-            sql2 = "insert into details (meal_id, amount, subtotal, state, wish_id_1, wish_id_2, wish_id_3, random_pick, order_id) values" +
+            let sql2 = "insert into details (meal_id, amount, subtotal, state, wish_id_1, wish_id_2, wish_id_3, random_pick, order_id) values" +
                    "(" + detail.meal_id + "," + detail.amount + "," + detail.subtotal + ", 1 ," + detail.wish_id_1 + "," + detail.wish_id_2 + "," + detail.wish_id_3 + "," +detail.random_pick + "," +orderId+ ");";
-            //console.log(sql2);
+            
             connection.query(sql2, (err, results) => {
                 if (err) {
                     throw err;
