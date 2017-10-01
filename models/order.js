5479ea8dfc4a62dd31c8c0b0d32934632c55e0a8
and85620 <and85620@gmail.com>
2017-10-01 10:00:16 +0800

    結帳api（感覺）完成 , 可進行測試

--- models/order.js
+++ models/order.js
@@ -2,23 +2,23 @@
 function newOrder (username, orderTime, total, details, callback) {
     // sql指令 -> 新增購物車
     let sql = "insert into orders  (user_id, order_time, total) values ('" + username + "','" + orderTime + "'," + total + ");";
-    console.log(sql);
+    //console.log(sql);
     connection.query(sql, (err, results) => {
         if (err) {
             throw err;
         }
-        console.log(results.insertId);
+        //console.log(results.insertId);
         let orderId = results.insertId;
-        
         for (let detail of details) {
-            sql = `insert into details (meal_id, amount, subtotal, state, wish_id_1, wish_id_2, wish_id_3, random_pick, order_id) values (${detail.meal_id}, ${detail.amount}, ${detail.subtotal}, 1, ${detail.whis_id_1}, ${detail.whis_id_2}, ${detail.whis_id_3}, ${detail.random_pick}, ${orderId});`;
-            connection.query(sql, (err, results) => {
+            sql2 = "insert into details (meal_id, amount, subtotal, state, wish_id_1, wish_id_2, wish_id_3, random_pick, order_id) values" +
+                   "(" + detail.meal_id + "," + detail.amount + "," + detail.subtotal + ", 1 ," + detail.wish_id_1 + "," + detail.wish_id_2 + "," + detail.wish_id_3 + "," +detail.random_pick + "," +orderId+ ");";
+            //console.log(sql2);
+            connection.query(sql2, (err, results) => {
                 if (err) {
                     throw err;
                 }
             });
         }
-
         callback(undefined, results);
         return;
     });
--- ordersRouter.js
+++ ordersRouter.js
@@ -1,48 +1,42 @@
-const express = require('express');
-const ordersRouter = express.Router();
-const moment = require('moment');
-const user = require('./models/user.js');
-const order = require('./models/order.js');
-const md5 = require('md5');
-
-ordersRouter.post('/', (req, res) => {
-    
-    let username = user.checkLogin(req.cookies);
-    let orderTime = moment().format('YYYY/MM/DD HH:mm:ss');
-    let total = req.body.total;
-    let details = req.body.details;
-    /*
-        details: [
-            {
-                meal_id,
-                amout,
-                subtotal,
-                //state(1),
-                wish_id_1,
-                wish_id_2,
-                wish_id_3,
-                random_pick,
-                //final_meal,
-                //order_id
-            }
-        ]
-    */
-
-    order.newOrder(username, orderTime, total, details, (error, results) => {
-        if (typeof(results) !== undefined && typeof(error) == "undefined") {
-            return res.status(200).json({
-                message: 'order successfully.'
-            });
-        } else {
-            return res.status(401).json({
-                error: error
-            });
-        }
-    });
-});
-
-ordersRouter.post('/', (req, res) => {
-    
-});
-
+const express = require('express'); 
+const ordersRouter = express.Router(); 
+const moment = require('moment'); 
+const user = require('./models/user.js'); 
+const order = require('./models/order.js'); 
+const md5 = require('md5'); 
+ 
+ordersRouter.post('/', (req, res) => { 
+     
+    let username = user.checkLogin(req.cookies); 
+    let orderTime = moment().format('YYYY/MM/DD HH:mm:ss'); 
+    let total = req.body.total; 
+    let details = req.body.details; 
+    /* 
+        details: [ 
+            { 
+                meal_id, 
+                amout, 
+                subtotal, 
+                //state(1), 
+                wish_id_1, 
+                wish_id_2, 
+                wish_id_3, 
+                random_pick, 
+                //final_meal, 
+                //order_id 
+            } 
+        ] 
+    */ 
+    order.newOrder(username, orderTime, total, details, (error, results) => { 
+        if (typeof(results) !== undefined && typeof(error) == "undefined") { 
+            return res.status(200).json({ 
+                message: 'order successfully.' 
+            }); 
+        } else { 
+            return res.status(401).json({ 
+                error: error 
+            }); 
+        } 
+    }); 
+}); 
 module.exports = ordersRouter;
