8cdef96bcd8d2920990b59e5b4201475e02fffba
and85620 <and85620@gmail.com>
2017-10-04 13:52:14 +0800

    店家資料 json 格式

--- models/shop.js
+++ models/shop.js
@@ -3,13 +3,39 @@ function showShop (callback) {
     // sql指令 -> 所有shop data
     let sql = `select shops.shop_id,shop_name,lowest_amount,highest_amount,shipping_fee,shop_discount,meals.meal_id,meal_name,meal_price,meal_discount from shops, meals where shops.shop_id = meals.shop_id Group by meals.meal_id;`;  
     connection.query(sql, (err, results) => {
+        let shops = {};
         if (err) {
             throw err;
         }
-        /*data = {
 
-        }*/
-        callback(undefined, results);
+        //hash 
+        for(let result of results) { // 讀每一筆 sql 查詢出來的資料
+            if(shops[result.shop_id] == null) {
+                // shop 未存在,增加店家
+                shops[result.shop_id] = {
+                    "shop_id": result.shop_id,
+                    "shop_name": result.shop_name,
+                    "lowest_amount": result.lowest_amount,
+                    "highest_amount": result.highest_amount,
+                    "shipping_fee": result.shipping_fee,
+                    "shop_discount": result.shop_discount
+                };
+            } 
+            // 最後再加餐點
+            // return answer ? "yes" : "no";
+            // answer is true then return "yes" , answer is false then return "no"
+            // shops[result.shop_id].meals ?= shops[result.shop_id].meals : {} ;
+            shops[result.shop_id].meals =  typeof shops[result.shop_id].meals === 'object' ? shops[result.shop_id].meals : {} ;
+            shops[result.shop_id].meals[result.meal_id] = {
+                "meal_id": result.meal_id,
+                "meal_name": result.meal_name,
+                "meal_price": result.meal_price,
+                "meal_discount": result.meal_discount
+            };
+            
+            
+        }
+        callback(undefined, shops);
         return;
     });
 }
@@ -17,3 +43,70 @@ function showShop (callback) {
 module.exports = {
     showShop
 };
+
+
+
+
+
+/*
+all_shops = {
+    1: {
+        "shop_id": 1,
+        "shop_name": "山園",
+        "lowest_amount": 15,
+        "highest_amount": 50,
+        "shipping_fee": 20,
+        "shop_discount": "0",
+        "meals": {
+            1: {
+                "meal_id": 1,
+                "meal_name": "雞腿飯",
+                "meal_price": 95,
+                "meal_discount": 0
+            }
+        }
+    },
+
+
+
+all_shops = {
+    "1": {
+        "shop_id": 1,
+        "shop_name": "山園",
+        "lowest_amount": 15,
+        "highest_amount": 50,
+        "shipping_fee": 20,
+        "shop_discount": "0",
+        "1": {
+            "meal_id": 1,
+            "meal_name": "雞腿飯",
+            "meal_price": 95,
+            "meal_discount": 0
+        },
+        "2":{
+
+        }
+    },
+
+
+let all_shops = {};
+results.forEach(function(element) {
+    all_shops[element.shop_id][element.meal_id] = {
+        "mael_name":    "雞腿飯",
+        "A__A":         "QAQ"
+    }
+
+    all_shops[element.shop_id] = {
+        "shop_name": "山園",
+        "lowest_amount": 15,
+        "highest_amount": 50,
+        "shipping_fee": 20,
+        "shop_discount": "0"
+    }
+
+    all_shops[element.shop_id].meals[element.meal_id] = {
+
+    }
+
+}, this);
+*/
