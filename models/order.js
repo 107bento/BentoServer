a4a2802113025e72150e18286433976a32146024
and85620 <and85620@gmail.com>
2017-10-26 14:37:36 +0800

    便當第一輪演算--> 下一步處理other

--- models/order.js
+++ models/order.js
@@ -25,7 +25,7 @@ function newOrder (username, orderTime, total, details, callback) {
 }
 
 // 排單演算法
-function sortOrder() { 
+function sortOrder() {
     // 建立今天的日期
     const date = moment().format('YYYY-MM-DD');
     // 搜尋所有當日的訂單
@@ -38,11 +38,21 @@ function sortOrder() {
         }
         // 清空所有店家
         let orderedShops = {};
-        // 看每一筆訂單資料
-        for (let result of results ) {
-            // 先把餐點數量存起來
-            let amount = result.amount;
 
+        // orderedShops[0] = other
+        orderedShops[0] = {
+            "shop_name" : "other",
+            "total_amount" : 0,
+            "lowest_amount": 0,
+            "highest_amount": 10000,
+            "meals" : {}
+        };
+
+        // 看每一筆訂單資料(第一輪)
+        for (let result of results ) {
+            // 先把餐點數量, 餐點Id, shop_id 存起來
+            let amount = result.amount;
+            let mealId = result.meal_id;
             let shop_id = result.shop_id;
             // 若 shop 未存在 => 增加店家
             if(orderedShops[shop_id] == null) {
@@ -54,37 +64,58 @@ function sortOrder() {
                     "meals": {}
                 };
             }
-
-            let mealId = result.meal_id;
-            // 若點家的 meal 不存在 => 新增餐點，並把數量存進去
+            // 若點家的 meal 不存在 && 加入不超過上限 => 新增餐點，並把數量存進去
+            // 若點家的 meal 不存在 but 加入會超過上限 => 丟進other 變成 state : F
             if(orderedShops[shop_id].meals[mealId] == null) {
-                if (amount <= orderedShops[shop_id].highest_amount) {
+                if (amount + orderedShops[shop_id].total_amount <= orderedShops[shop_id].highest_amount) {
                     orderedShops[shop_id].meals[mealId] = {
                         amount,
-                        "details": [result.detail_id]
+                        // detail_id =>誰訂, state =>確定了沒, round=>第幾輪
+                        // T 1 排第1輪就確定, F 2 排第2輪但不成功要進第3輪
+                        "details": [ { "result.detail_id" : result.detail_id, "subamount" : amount, "state" : "F", "round" : 1} ]
                     };
                     orderedShops[shop_id].total_amount += amount;
+                } else {
+                    if (orderedShops[0].meals[mealId] == null) {
+                        orderedShops[0].meals[mealId] = {
+                            amount,
+                            "details": [ { "result.detail_id" : result.detail_id, "subamount" : amount, "state" : "F", "round" : 1} ]
+                        };
+                    } else {
+                        orderedShops[0].meals[mealId].amount += amount;
+                        orderedShops[0].meals[mealId].details.push( { "result.detail_id" : result.detail_id, "subamount" : amount, "state": "F", "round" : 1 } ) ;
+                    }
+                    orderedShops[0].total_amount += amount;
                 }
-            } else {
-                amount += result.meal_amount;
-                // 已經存在就累加
-                if (amount <= orderedShops[shop_id].highest_amount) {
-                    
-                    orderedShops[shop_id].meals[mealId].amount = amount;
-                    orderedShops[shop_id].meals[mealId].details.push(result.detail_id);
+            } else {  // 店家存在 && 餐點也存在
+                // amount += result.meal_amount;(不確定-無義)
+                // 檢查加入會不會超過上限
+                // OK => 存紀錄（mealId.amount, datail_id, total_amount)
+                // 會爆 => 丟other & 存紀錄
+                if (orderedShops[shop_id].total_amount + result.meal_amount <= orderedShops[shop_id].highest_amount) {
+                    orderedShops[shop_id].meals[mealId].amount += amount;
+                    orderedShops[shop_id].meals[mealId].details.push( { "result.detail_id" : result.detail_id, "subamount" : amount, "state": "F", "round" : 1 } );
                     orderedShops[shop_id].total_amount += amount;
+                } else {
+                    if (orderedShops[0].meals[mealId] == null) {
+                        orderedShops[0].meals[mealId] = {
+                            amount,
+                            "details": [ { "result.detail_id" : result.detail_id, "subamount" : amount, "state" : "F", "round" : 1} ]
+                        };
+                    } else {
+                        orderedShops[0].meals[mealId].amount += amount;
+                        orderedShops[0].meals[mealId].details.push( { "result.detail_id" : result.detail_id, "subamount" : amount, "state": "F", "round" : 1 } ) ;
+                    }
+                    orderedShops[0].total_amount += amount;
                 }
-            }
-            
-            
+            }  
         }
-        console.log(orderedShops['1'].meals['2']);
+        // console.log(orderedShops['1'].meals['2']);
         // 
     });
 }
 
 module.exports = {
     newOrder,
-    sortOrder,
-    _belongShop
+    sortOrder
 };
