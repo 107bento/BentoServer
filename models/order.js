340e6db4e9326138235b36a9e3864c45b1807242
SafeFly <ast850328@gmail.com>
2017-11-05 01:29:58 +0800

    把 wish 從 array 拆成獨立的欄位

--- models/order.js
+++ models/order.js
@@ -11,7 +11,7 @@ function newOrder (username, orderTime, total, details, callback) {
         let orderId = results.insertId;
         for (let detail of details) {
             let sql2 = "insert into details (meal_id, amount, subtotal, state, wish_id_1, wish_id_2, wish_id_3, random_pick, order_id) values" +
-                   "(" + detail.meal_id + "," + detail.amount + "," + detail.subtotal + ", 1 ," + detail.wish_id[0] + "," + detail.wish_id[1] + "," + detail.wish_id[2] + "," +detail.random_pick + "," +orderId+ ");";
+                   "(" + detail.meal_id + "," + detail.amount + "," + detail.subtotal + ", 1 ," + detail.wish_id_1 + "," + detail.wish_id_2 + "," + detail.wish_id_3 + "," +detail.random_pick + "," +orderId+ ");";
             
             connection.query(sql2, (err, results) => {
                 if (err) {
