6d0acdb24a03fe5b319aa6c855dbe73e516e9212
SafeFly <ast850328@gmail.com>
2017-11-09 23:00:49 +0800

    儲值的 model ，資料庫記得改一下架構

--- models/user.js
+++ models/user.js
@@ -86,10 +86,47 @@ function checkLogin(reqCookie) {
     }
 }
 
+// 儲值
+function storeValue(username, value, callback) {
+    
+    // transaction 如果有一個 error 全部 rollback
+    connection.beginTransaction((err) => {
+        if (err) { 
+            callback({"error": "Something went wrong."}, undefined);
+            throw err; 
+        }
+
+        // 先把帳號裡的錢加上去
+        let sql = 'update users set money = money + ' + value + ' where user_id = "' + username +'";';
+        connection.query(sql, (err, results) => {
+            if (err) {
+                callback({"error": "Something went wrong."}, undefined);
+                return connection.rollback(function() {
+                    throw err;
+                });
+            }
+            // 紀錄該筆 record
+            let date = moment().format('YYYY-MM-DD');
+            sql = 'insert into records (date, remain, user_id, value) VALUES ("'+ date +'", (select money from users where user_id = "' + username + '"), "' + username + '", ' + value + ');';
+            connection.query(sql, (err, results) => {
+                if (err) {
+                    callback({"error": "Something went wrong."}, undefined);
+                    return connection.rollback(function() {
+                        throw err;
+                    });
+                }
+                callback(undefined, {"success": "store successfully!"});
+                return;
+            });
+        });
+    });
+}
+
 module.exports = {
     validate,
     register,
     modify,
     showUser,
-    checkLogin
+    checkLogin,
+    storeValue
 };
