c0a759ec231e7cf7f6b0502addb3172f0bd9029b
SafeFly <ast850328@gmail.com>
2017-12-27 22:09:48 +0800

    POST /login/shops 店家登入 route

--- loginRouter.js
+++ loginRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const loginRouter = express.Router();
 const user = require('./models/user');
+const shop = require('./models/shop');
 const moment = require('moment');
 const md5 = require('md5');
 
@@ -37,6 +38,34 @@ loginRouter.post('/', (req, res) => {
             return res.status(400).json({
                 error: error,
                 "status_code" : 400
+            });
+        }
+    });
+});
+
+// 店家登入
+loginRouter.post('/shops', (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(400).json({
+            error: 'lost username or password.'
+        });
+    }
+
+    shop.validate(username, password, (error, results) => {
+        if (typeof(results) !== undefined && typeof(error) == "undefined") {
+            // session id 加密 （username + 時間戳ㄋ）
+            let uuid = md5(results.user_id + moment());
+            
+            _shopCookies[uuid] = results.username;
+            res.cookie(shopCookieName, uuid);
+            return res.status(200).json(results);
+        } else {
+            return res.status(400).json({
+                error: error
             });
         }
     });
